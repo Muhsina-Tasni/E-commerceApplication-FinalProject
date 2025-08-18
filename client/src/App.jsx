52a5fe9bf5aab62d0d6c0ea322ea026c412013ee
Muhsina-Tasni <tasnimuhsina@gmail.com>
2025-08-18 18:27:59 +0530

    dding diagram

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,17 @@
-
+import {BrowserRouter, Routes} from "react-router-dom"
 import './App.css'
 
 function App() {
 
 
   return (
-    <>
-      
-    </>
+    
+      <BrowserRouter>
+      <Routes>
+        {/* <Route></Route> */}
+      </Routes>
+      </BrowserRouter>
+   
   )
 }
 
